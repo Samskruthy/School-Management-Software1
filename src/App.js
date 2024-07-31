ad2650176122480d18152b7bc3965b416580a2aa
Samskruthy <tysamskruthy@gmail.com>
2024-07-31 15:30:07 +0530

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
 
+import React from "react";
+import "./style.css";
+import Management from './Management.js'
+
+export default function App() {
+  return (
+    <div>
+      <Management/>
+    </div>
+  );
+}
+
